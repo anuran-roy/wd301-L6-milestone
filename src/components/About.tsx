0545e7dd78ea3d16a693ee4d46007a1e00f482ec
anuran <anuranroy02@gmail.com>
2022-04-17 17:02:27 +0530

    Fixed Counter not loading

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,12 @@
 import AppContainer from "./AppContainer";
 import Header from "./Header";
-import { Counter } from "./mock/Counter";
+// import { Counter } from "./mock/Counter";
 
 export default function About() {
   return (
     <AppContainer>
       <Header title="About Page" />
-      <Counter />
+      {/* <Counter /> */}
       <div className="my-5 mx-2 flex justify-center py-5 px-2">
         <h1 className="text-4xl font-bold">
           Filler Page for practicing Raviger :D!
